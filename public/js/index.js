e0fefd83b35d69e027cc67c4839d733db3787e91
Kinyugo <kinyugokyde@gmail.com>
2019-09-04 22:10:16 +0300

    Add form for messages and show incoming messages in the browser

--- public/js/index.js
+++ public/js/index.js
@@ -2,7 +2,6 @@ const socket = io();
 
 socket.on("connect", () => {
   console.log("Connected to the server.");
-
 });
 
 socket.on("disconnect", () => {
@@ -11,4 +10,26 @@ socket.on("disconnect", () => {
 
 socket.on("newMessage", message => {
   console.log("New Message.", message);
+
+  let li = $("<li></li>");
+  li.text(`${message.from} : ${message.text}`)
+
+  $("#messages").append(li);
+});
+
+$("#message-form").on("submit", e => {
+    const messageInput = $("[name=message]");
+  e.preventDefault();
+  socket.emit(
+    "createMessage",
+    {
+      from: "user",
+      text: messageInput.val()
+    },
+    (data) => {
+        console.log("Server says: ", data);
+    }
+  );
+
+  messageInput.val("");
 });
--- server/server.js
+++ server/server.js
@@ -28,11 +28,12 @@ io.on("connection", socket => {
 
   socket.broadcast.emit("newMessage", generateMessage("Admin", "New user joined."));
 
-  socket.on("createMessage", newMessage => {
+  socket.on("createMessage", (newMessage, callback) => {
     console.log("Create Message: ", newMessage);
 
     io.emit("newMessage", generateMessage(newMessage.from, newMessage.text));
 
+    callback("From back here.");
     // socket.broadcast.emit("newMessage", {
     //   from: newMessage.from,
     //   text: newMessage.text,
